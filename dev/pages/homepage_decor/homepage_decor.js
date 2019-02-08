72fb5998374e5ed84d4afd36845472766a2ef1c3
Anastasia Abramenko <nasya.abramenko@gmail.com>
2019-02-08 12:27:46 +0200

    last changes

--- dev/pages/homepage_decor/homepage_decor.js
+++ dev/pages/homepage_decor/homepage_decor.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 			$('.slick-slider ul').slick({
 				slidesToShow: 4,
 				arrows: true,
-				autoplay: true,
-				autoplaySpeed: 1000,
+				// autoplay: true,
+				// autoplaySpeed: 1000,
 			});
 		});
 
